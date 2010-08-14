610e74c5b48884e5f8549ece25c3fefa85292e80
cristian <cristian@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2010-08-14 12:47:56 +0000

    moving to trunk

---
+++ database/tigo_db.sql
@@ -0,0 +1,7 @@
+--
+-- Create schema tigo_videocms_db
+--
+CREATE DATABASE IF NOT EXISTS tigo_videocms_db;
+USE tigo_videocms_db;
+
+GRANT all ON tigo_videocms_db.* TO 'tigo'@'localhost' IDENTIFIED BY 'java1234';
