09ecb8bb8329f022213f13d351ddd81d9c74e49c
Mariano Capurro <marianocapurro@users.noreply.github.com>
2010-08-16 15:08:24 +0000

    Added FTP and SFTP Ant dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,16 +16,24 @@ grails.project.dependency.resolution = {
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenLocal()
+        mavenCentral()
+        mavenRepo "http://snapshots.repository.codehaus.org"
+        mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.5'
+		runtime 'org.apache.ant:ant-jsch:1.7.1',
+				'com.jcraft:jsch:0.1.42',
+				'org.apache.ant:ant-commons-net:1.7.1',
+				'commons-net:commons-net:2.0'
+		test 'org.apache.ant:ant-jsch:1.7.1',
+ 			 'com.jcraft:jsch:0.1.42',
+			 'org.apache.ant:ant-commons-net:1.7.1',
+			 'commons-net:commons-net:2.0'
     }
 }
