dde7336e29bf5fd2b01119806a91357173f247aa
Mariano Capurro <marianocapurro@users.noreply.github.com>
2010-08-16 15:09:01 +0000

    Added file uploader service (FTP & SFTP)

---
+++ grails-app/services/com/tigo/videocms/FileUploaderService.groovy
@@ -0,0 +1,28 @@
+package com.tigo.videocms
+
+class FileUploaderService {
+    static transactional = false
+	
+    def sendFileBySFTP(String host, String user, String password, String srcDir, String srcFile, String destDir) {
+		def ant = new AntBuilder()
+		
+		ant.scp(trust:'true',file:"${srcDir}/${srcFile}",
+			todir:"${user}@${host}:${destDir}",
+			password:"${password}",verbose:"false");
+    }
+	
+	def sendFileByFTP(String host, String user, String password, String srcDir, String srcFile, String destDir) {
+		def ant = new AntBuilder()
+
+		ant.ftp( server:"${host}",
+			userid:"${user}",
+			password:"${password}",
+			passive:"yes",
+			verbose:"no",
+			remotedir:"${destDir}",
+			binary:"yes" ) {
+				fileset( dir:"${srcDir}" ) { include( name:"${srcFile}" ) }
+			}
+	}
+	
+}
