b75703659cabe7b6fd7c16ce4539188672663c64
mariano <mariano@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2010-08-16 15:09:45 +0000

    Added permissions to every target

--- project/TIGO_videocms/trunk/database/tigo_db.sql
+++ project/TIGO_videocms/trunk/database/tigo_db.sql
@@ -5,3 +5,4 @@ CREATE DATABASE IF NOT EXISTS tigo_videocms_db;
 USE tigo_videocms_db;
 
 GRANT all ON tigo_videocms_db.* TO 'tigo'@'localhost' IDENTIFIED BY 'java1234';
+GRANT all ON tigo_videocms_db.* TO 'tigo'@'%' IDENTIFIED BY 'java1234';
