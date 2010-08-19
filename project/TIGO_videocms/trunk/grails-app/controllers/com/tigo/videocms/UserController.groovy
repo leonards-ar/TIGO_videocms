1e9b672698c116e00663f133b779f620fa68519c
cristian <cristian@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2010-08-19 22:31:49 +0000

    Adding update roles to User ABM Adding password change to User ABM

--- project/TIGO_videocms/trunk/grails-app/controllers/com/tigo/videocms/UserController.groovy
+++ project/TIGO_videocms/trunk/grails-app/controllers/com/tigo/videocms/UserController.groovy
@@ -1,6 +1,8 @@
 package com.tigo.videocms
 
 import grails.plugins.springsecurity.Secured
+import org.springframework.validation.BindException
+import java.util.Set;
 
 @Secured(['ROLE_ADMIN'])
 class UserController {
@@ -22,53 +24,55 @@ class UserController {
 		def userInstance = new User()
 		userInstance.properties = params		
 		def defaultRole = SecRole.findByAuthority('ROLE_BACKOFFICE_USER').getId()		
-		return [userInstance: userInstance, role:defaultRole]
+		return [userInstance: userInstance, roles:defaultRole]
 	}
 	
 	def save = {
-		def userInstance = new User(params)
-		def roleAux = request.getParameter('role')
+		def errorFlag = false
+			
+		def userInstance = new User(params)	
 		
-		def passwordConfirmation = request.getParameter('passwordConfirmation')
-		def userPassword = userInstance?.getPassword()
+		if (!userInstance.validate())
+		{
+			errorFlag = true
+		}	
 		
-		if (userPassword) {
-			if(!passwordConfirmation || !(userPassword?.equals(passwordConfirmation))){
-				userInstance.errors.rejectValue("password", "user.error.password.confirmation.doesnotmatch","Password does not match confirmation")
-				render(view: "create", model: [userInstance: userInstance, role:roleAux])
-				return
-			}else{
+		String[] rolesAux = request.getParameterValues('roles')
+		def confirmationPassword = request.getParameter('confirmationPassword')
+		
+		def userPassword = userInstance.getPassword()
+		
+		if (userPassword){
+			if (validatePassword(userPassword, confirmationPassword)){
 				def auxPassword = springSecurityService.encodePassword(userPassword)
 				userInstance.setPassword(auxPassword)
+			}else{
+				userInstance.errors.rejectValue("password", "user.error.password.confirmation.doesnotmatch","Password does not match confirmation")
+				errorFlag = true
 			}
 		}
 		
+		//TODO: Check that the user is not admin		
 		if(userInstance?.getCountries()?.size() < 1){
 			userInstance.errors.rejectValue("countries", "user.error.nocountry","Country cannot be empty")
-			render(view: "create", model: [userInstance: userInstance, role:roleAux])
-			return
+			errorFlag = true
 		}
 
-		if(!roleAux){
-			userInstance.errors.rejectValue("role", "user.error.norole","Role cannot be empty")
-			render(view: "create", model: [userInstance: userInstance, role:roleAux])
-			return
+		if(!rolesAux){
+			errors = new BindException(this, "com.tigo.videocms.UserController")
+			errors.reject("user.error.norole","Roles cannot be empty")	
+			errorFlag = true
 		}
 
 		userInstance.setEnabled(true)
 		
-		if (userInstance.save(flush: true)) {
-			def userRole = SecRole.findById(roleAux)
-			
-			if (!userInstance.authorities.contains(userRole)) {
-				SecUserSecRole.create userInstance, userRole
-			}
-						
+		if (!errorFlag && userInstance.save(flush: true)) {
+			addRoles(rolesAux, userInstance)
 			flash.message = "${message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])}"
 			redirect(action: "show", id: userInstance.id)
 		}
 		else {
-			render(view: "create", model: [userInstance: userInstance, role:roleAux])
+			render(view: "create", model: [userInstance: userInstance, roles:rolesAux])
 		}
 	}
 	
@@ -90,31 +94,88 @@ class UserController {
 			redirect(action: "list")
 		}
 		else {
-			return [userInstance: userInstance]
+			def rolesAux = userInstance.getAuthorities().collect {it.id} as Set
+			return [userInstance: userInstance, roles: rolesAux]
 		}
 	}
 	
 	def update = {
+		def errorFlag = false 		
 		def userInstance = User.get(params.id)
+		String[] rolesAux = request.getParameterValues('roles')
+		
 		if (userInstance) {
 			if (params.version) {
 				def version = params.version.toLong()
 				if (userInstance.version > version) {
 					
 					userInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'user.label', default: 'User')] as Object[], "Another user has updated this User while you were editing")
-					render(view: "edit", model: [userInstance: userInstance])
+					render(view: "edit", model: [userInstance: userInstance, roles: rolesAux])
 					return
 				}
 			}
-			
+						
 			userInstance.properties = params
 			
-			if (!userInstance.hasErrors() && userInstance.save(flush: true)) {
+			if (!userInstance.validate())
+			{
+				errorFlag = true
+			}
+			
+			def oldPassword = request.getParameter('oldPassword')
+			def newPassword = request.getParameter('newPassword')
+			def confirmationPassword = request.getParameter('confirmationPassword')
+			
+			if (oldPassword || newPassword || confirmationPassword)
+			{
+				//check if all the values are defined
+				if (oldPassword && newPassword && confirmationPassword){
+					//check if old password matches user password
+					def userPassword = userInstance.getPassword()
+					def oldPasswordEncoded = springSecurityService.encodePassword(oldPassword)
+					
+					if (validatePassword(userPassword, oldPasswordEncoded)){					
+						//check if new password matches confirmation
+						if (validatePassword(userPassword, oldPasswordEncoded)){					
+							def auxPassword = springSecurityService.encodePassword(newPassword)
+							userInstance.setPassword(auxPassword)
+						}else{
+							errors = new BindException(this, "com.tigo.videocms.UserController")						
+							errors.reject("user.error.password.confirmation.doesnotmatch","Password does not match confirmation")
+							errorFlag = true
+						}
+					}else{
+						errors = new BindException(this, "com.tigo.videocms.UserController")
+						errors.reject("user.error.oldpassword.newpassword.doesnotmatch","Old password is invalid")
+						errorFlag = true
+					}
+				}else{
+					errors = new BindException(this, "com.tigo.videocms.UserController")				
+					errors.reject("user.error.passwordforupdate.mandatory","To modify the password please enter: old password, new password, and password confirmation")
+					errorFlag = true									
+				}
+			}
+			
+			if(userInstance?.getCountries()?.size() < 1){
+				userInstance.errors.rejectValue("countries", "user.error.nocountry","Country cannot be empty")
+				errorFlag = true
+			}
+	
+			if(!rolesAux){
+				errors = new BindException(this, "com.tigo.videocms.UserController")
+				errors.reject("user.error.norole","Role cannot be empty")
+				errorFlag = true
+			}
+	
+			if (!errorFlag && userInstance.save(flush: true)) {
+				SecUserSecRole.removeAll(userInstance)
+				addRoles(rolesAux, userInstance)
+								
 				flash.message = "${message(code: 'default.updated.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])}"
 				redirect(action: "show", id: userInstance.id)
 			}
 			else {
-				render(view: "edit", model: [userInstance: userInstance])
+				render(view: "edit", model: [userInstance: userInstance,roles:rolesAux])
 			}
 		}
 		else {
@@ -143,4 +204,19 @@ class UserController {
 			redirect(action: "list")
 		}
 	}
+	
+	def validatePassword(String userPassword, String passwordConfirmation){
+		return (userPassword && passwordConfirmation && (userPassword.equals(passwordConfirmation)))
+	}
+	
+	def addRoles(String[] rolesAux, User userInstance){		
+		rolesAux.each {
+			def userRole = SecRole.findById(it)
+			
+			if (!userInstance.authorities.contains(userRole)) {
+				SecUserSecRole.create userInstance, userRole, true
+			}
+		}
+	}
+
 }
