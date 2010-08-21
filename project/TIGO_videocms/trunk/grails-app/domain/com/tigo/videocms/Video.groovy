4e61585f351f8cd8a5ba0ecdc25752473e32e596
cristian <cristian@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2010-08-21 19:20:49 +0000

    testing post-commit hook to activate hudson build

--- project/TIGO_videocms/trunk/grails-app/domain/com/tigo/videocms/Video.groovy
+++ project/TIGO_videocms/trunk/grails-app/domain/com/tigo/videocms/Video.groovy
@@ -20,7 +20,10 @@ class Video {
 	Date uploadDate = new Date()
 	
 	AudienceRestriction audienceRestriction
-	
+
+	//if countries is null the minSize validation won't kick off.
+	//Need to add constructor since grails does not create a default Set
+	//See http://jira.codehaus.org/browse/GRAILS-2808
 	Set countries = new HashSet()
 	Set categories = new HashSet()
 	
